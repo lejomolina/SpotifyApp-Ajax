bd4528da3863bd193eab69f5c0618ffb7c96f47b
lejomolina <lejomolina@gmail.com>
2020-07-14 11:33:28 -0500

    Se definieron todos los objetos Ajax

--- control.js
+++ control.js
@@ -8,7 +8,7 @@ let URL= "https://api.spotify.com/v1/artists/1w5Kfo2jwwIPruYS2UWh56/top-tracks?c
 
 // 2. Definir el token o permiso de acceso
 
-let token = "Bearer BQDCKfiTDObZJw_FHgkz0bghucXaS7DS0bm4-L43_wSUYj8PkahtgBPrcdnOJhwvT_5JJT9kCrsCqYhNr2lyDWgJjcO7CqSeYLeNU_6XUjvAyqHtsq5rvKlbkibdUgF8rhuCmt2Y0cfSUoimduK1ab3jNoxxqrWrqiU"
+let token = "Bearer BQCADXhoWkptYNyzhZ_aNcueX60OXhvfkIWzJEJUk0DGSAWWoAQEA48eG-6R_-U0u71Fq3RXYd0GZVH64vIPvKxpTeq9XQFETDhGcAWHHSyzAiXIsVUH52xQ0LL8ez60Mz-yJhqPIfVy2ZKD3dms5uSpJuiaPhZfui4";
 
 // 3. Definir el objeto AJAX
 
@@ -25,11 +25,68 @@ obAJAX.setRequestHeader('Authorization', token);
 
 obAJAX.onload = function() {
     let respuesta = JSON.parse(this.responseText);
+    
+    /*Se depura la localización de la imagen. La dejo así para en caso de que se me olvide, 
+    venir a este código y consultar...
     console.log(respuesta);
     console.log(respuesta.tracks);
     console.log(respuesta.tracks[0]);
+    console.log(respuesta.tracks[0].album);
+    console.log(respuesta.tracks[0].album.images);
+   
+    //Estos console.log se crearon para probar si efectivamente se están trayendo los servicios de la web.
+    console.log(respuesta.tracks[0].album.images[0].url);
+    console.log(respuesta.tracks[0].album.name);
+    console.log(respuesta.tracks[0].preview_url);
+
+    console.log(respuesta.tracks[1].album.images[0].url);
+    console.log(respuesta.tracks[1].album.name);
+    console.log(respuesta.tracks[1].preview_url);
+        
+    console.log(respuesta.tracks[2].album.images[0].url);
+    console.log(respuesta.tracks[2].album.name);
+    console.log(respuesta.tracks[2].preview_url);*/
+
+    //Se crean las variables para llamar a las etiquetas.
+    let imagen0 = document.getElementById("imgjam0");
+    let titulo0 = document.getElementById("namejam0");
+    let audio0 = document.getElementById("sonidojam0");
+
+    //Ya se tiene la etiqueta en la variable, ahora se le da al atributo SRC, el contenido de la URL que tiene el servicio.  
+    imagen0.src=respuesta.tracks[0].album.images[0].url;
+    titulo0.textContent = respuesta.tracks[0].album.name
+    audio0.src= respuesta.tracks[0].preview_url;
+
 }
 
+let obAJAX1= new XMLHttpRequest();
+obAJAX1.open('GET', URL, true);
+obAJAX1.setRequestHeader('Authorization', token);
+obAJAX1.onload = function() {
+        let respuesta1 = JSON.parse(this.responseText);
+
+    let imagen1 = document.getElementById("imgjam1");
+    let titulo1 = document.getElementById("namejam1");
+    let audio1 = document.getElementById("sonidojam1");
+
+    imagen1.src=respuesta1.tracks[1].album.images[0].url;
+    titulo1.textContent = respuesta1.tracks[1].album.name;
+    audio1.src= respuesta1.tracks[1].preview_url;
+}
+
+let obAJAX2 = new XMLHttpRequest();
+obAJAX2.open('GET', URL, true);
+obAJAX2.setRequestHeader('Authorization', token);
+obAJAX2.onload = function() {
+    let respuesta2 = JSON.parse(this.responseText);
+
+    let imagen2 = document.getElementById("imgjam2");
+    let titulo2 = document.getElementById("namejam2");
+    let audio2 = document.getElementById("sonidojam2");
+
+    imagen2.src=respuesta2.tracks[2].album.images[0].url;
+    titulo2.textContent = respuesta2.tracks[2].album.name;
+    audio2.src= respuesta2.tracks[2].preview_url;
+}
 //7. Enviamos la petición.
 
-obAJAX.send();
